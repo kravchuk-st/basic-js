f4a892bd994b61fca66b55de99c480d878332b26
kravchuk-st <57140996+kravchuk-st@users.noreply.github.com>
2022-04-30 16:42:14 +0300

    fix bug

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
+function createDreamTeam(arr) {
   if(!Array.isArray(arr)) return false;
   
   let team = [];
