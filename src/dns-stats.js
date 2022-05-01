ed27d407f5416d719ae8c8aa53869b0e73618bf0
kravchuk-st <57140996+kravchuk-st@users.noreply.github.com>
2022-05-01 14:47:27 +0300

    fix bug

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,7 +22,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
+function getDNSStats(domains) {
   const res = {};
 	
 	if (domains.length === 0) return res;
